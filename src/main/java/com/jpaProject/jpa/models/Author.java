8320754b6861fce9e0f04f92d4e060de935e8070
kimtaehyeung <108637691+riumr@users.noreply.github.com>
2024-05-26 22:03:44 +0900

    feat : Column별 속성 추가

--- src/main/java/com/jpaProject/jpa/models/Author.java
+++ src/main/java/com/jpaProject/jpa/models/Author.java
@@ -1,9 +1,14 @@
 package com.jpaProject.jpa.models;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.TableGenerator;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,13 +18,20 @@ import lombok.NoArgsConstructor;
 public class Author {
     @Id
     @GeneratedValue
+    // (strategy = GenerationType.TABLE,generator = "author_id_gen")
+    // @SequenceGenerator(name = "author_sequence",sequenceName = "author_sequence",allocationSize = 1)
+    // @TableGenerator(name = "author_id_gen", table = "id_generator",
+    //                 pkColumnName = "id_name",valueColumnName = "id_value",allocationSize = 1)
     private Integer id;
-    @Column
+    @Column(name = "f_name",length = 35)
     private String firstName;
-    @Column
+    
     private String lastName;
-    @Column
+    @Column(unique=true, nullable = false)
     private String email;
-    @Column
     private int age;
+    @Column(updatable = false, nullable = false)
+    private LocalDate createdAt;
+    @Column(insertable = false)
+    private LocalDate lastModified;
 }
