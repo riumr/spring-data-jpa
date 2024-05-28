5f666b1abeede6eed79ff9994d71d73bb549b603
kimtaehyeung <108637691+riumr@users.noreply.github.com>
2024-05-28 22:26:17 +0900

    feat : 관계 구현할 Entity들 생성
    - Course, Lecture, Resource, Section

---
+++ src/main/java/com/jpaProject/jpa/models/Course.java
@@ -0,0 +1,22 @@
+package com.jpaProject.jpa.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue
+    private Integer Id;
+    private String name;
+    private String description;
+}
