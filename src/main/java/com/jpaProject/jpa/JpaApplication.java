89957ddae2bb08b8a3a1bec3dff79e23255a3e3d
kimtaehyeung <108637691+riumr@users.noreply.github.com>
2024-05-29 22:11:56 +0900

    feat : Bean 주석처리

--- src/main/java/com/jpaProject/jpa/JpaApplication.java
+++ src/main/java/com/jpaProject/jpa/JpaApplication.java
@@ -15,7 +15,7 @@ public class JpaApplication {
 		SpringApplication.run(JpaApplication.class, args);
 	}
 
-	@Bean
+	// @Bean
 	public CommandLineRunner commandLineRunner(AuthorRepository repository){
 		return args->{
 			var author = Author.builder()
