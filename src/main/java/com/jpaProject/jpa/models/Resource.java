e89212e246556bb83e6a5b8a1e8d2f08458bfbae
kimtaehyeung <108637691+riumr@users.noreply.github.com>
2024-06-03 23:12:05 +0900

    feat : Joined 상속 타입 적용

--- src/main/java/com/jpaProject/jpa/models/Resource.java
+++ src/main/java/com/jpaProject/jpa/models/Resource.java
@@ -18,8 +18,8 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @SuperBuilder
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "resource_type")
+@Inheritance(strategy = InheritanceType.JOINED)
+// @DiscriminatorColumn(name = "resource_type") --> Only with SINGLE_TABLE
 public class Resource {
 
     @Id
