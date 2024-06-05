a4b082a85d54c02ca67cc4daddb4cfc80e9e8a93
kimtaehyeung <108637691+riumr@users.noreply.github.com>
2024-06-05 21:33:33 +0900

    feat : JpaRepository naming convention에 따라 메소드 생성

--- src/main/java/com/jpaProject/jpa/repository/AuthorRepository.java
+++ src/main/java/com/jpaProject/jpa/repository/AuthorRepository.java
@@ -1,9 +1,11 @@
 package com.jpaProject.jpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jpaProject.jpa.models.Author;
 
 public interface AuthorRepository extends JpaRepository<Author,Integer> {
-
+    List<Author> findAllByFirstName(String fn);
 }
