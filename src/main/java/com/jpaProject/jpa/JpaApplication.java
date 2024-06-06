34ac43ebe38888f2b2c8f7725819e360c2428e47
kimtaehyeung <108637691+riumr@users.noreply.github.com>
2024-06-06 22:09:58 +0900

    feat : faker 메소드로 DB에 임의값 추가

--- src/main/java/com/jpaProject/jpa/JpaApplication.java
+++ src/main/java/com/jpaProject/jpa/JpaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.github.javafaker.Faker;
 import com.jpaProject.jpa.models.Author;
 import com.jpaProject.jpa.models.Video;
 import com.jpaProject.jpa.repository.AuthorRepository;
@@ -20,18 +21,21 @@ public class JpaApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AuthorRepository repository,VideoRepository videoRepository){
 		return args->{
-			// var author = Author.builder()
-			// 			.firstName("John")
-			// 			.lastName("Willam")
-			// 			.age(23)
-			// 			.email("e@mail.com")
-			// 			.build();
-			// repository.save(author);
-			var video = Video.builder()
-				.name("abc")
-				.length(5)
-				.build();
-			videoRepository.save(video);
+			for(int i=0; i<50; i++){
+				Faker faker = new Faker();
+				var author = Author.builder()
+							.firstName(faker.name().firstName())
+							.lastName(faker.name().lastName())
+							.age(faker.number().numberBetween(19, 50))
+							.email("e"+i+"@mail.com")
+							.build();
+				repository.save(author);
+			}
+			// var video = Video.builder()
+			// 	.name("abc")
+			// 	.length(5)
+			// 	.build();
+			// videoRepository.save(video);
 		};
 	}
 
