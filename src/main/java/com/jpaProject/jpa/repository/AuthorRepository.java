c32477efce92b3b8e543463d8ede17e8ab52115f
kimtaehyeung <108637691+riumr@users.noreply.github.com>
2024-06-06 22:09:05 +0900

    feat : JpaRepository custom 메소드들 추가

--- src/main/java/com/jpaProject/jpa/repository/AuthorRepository.java
+++ src/main/java/com/jpaProject/jpa/repository/AuthorRepository.java
@@ -7,5 +7,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jpaProject.jpa.models.Author;
 
 public interface AuthorRepository extends JpaRepository<Author,Integer> {
+    // select * from Author where first_name = fn
     List<Author> findAllByFirstName(String fn);
+    
+    // select * from Author where first_name = fn
+    List<Author> findAllByFirstNameIgnoreCase(String fn);
+    
+    // select * from Author where first_name like '%fn%'
+    List<Author> findAllByFirstNameContainingIgnoreCase(String fn);
+
+    // select * from Author where first_name like 'fn%'
+    List<Author> findAllByFirstNameStartsWithIgnoreCase(String fn);
+
+    // select * from Author where first_name like '%fn'
+    List<Author> findAllByFirstNameEndsWithIgnoreCase(String fn);
+
+    // select * from Author where first_name in (...)
+    List<Author> findAllByFirstNameInIgnoreCase(List<String> firstNames);
 }
