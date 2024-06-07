f34920cd6f6dfa0b3a5b818eaf4567ec14f83b42
kimtaehyeung <108637691+riumr@users.noreply.github.com>
2024-06-07 22:51:26 +0900

    feat : query annotation으로 transaction 메서드 구현

--- src/main/java/com/jpaProject/jpa/JpaApplication.java
+++ src/main/java/com/jpaProject/jpa/JpaApplication.java
@@ -21,21 +21,29 @@ public class JpaApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(AuthorRepository repository,VideoRepository videoRepository){
 		return args->{
-			// for(int i=0; i<50; i++){
-			// 	Faker faker = new Faker();
-			// 	var author = Author.builder()
-			// 				.firstName(faker.name().firstName())
-			// 				.lastName(faker.name().lastName())
-			// 				.age(faker.number().numberBetween(19, 50))
-			// 				.email("e"+i+"@mail.com")
-			// 				.build();
-			// 	repository.save(author);
-			// }
-			// var video = Video.builder()
-			// 	.name("abc")
-			// 	.length(5)
-			// 	.build();
-			// videoRepository.save(video);
+			for(int i=0; i<50; i++){
+				Faker faker = new Faker();
+				var author = Author.builder()
+							.firstName(faker.name().firstName())
+							.lastName(faker.name().lastName())
+							.age(faker.number().numberBetween(19, 50))
+							.email(faker.name().username()+i+"@mail.com")
+							.build();
+				repository.save(author);
+			}
+			// update Author with ID = 1
+				var author = Author.builder()
+							.id(1)
+							.firstName("john")
+							.lastName("son")
+							.age(30)
+							.email("new@mail.com")
+							.build();
+				// repository.save(author);
+			// update Author set a.age = 25 where a.id = 1
+			repository.updateAuthor(25, 1);
+			// update Author set a.age = 24
+			repository.updateAllAuthorsAge(24);
 		};
 	}
 
--- src/main/java/com/jpaProject/jpa/repository/AuthorRepository.java
+++ src/main/java/com/jpaProject/jpa/repository/AuthorRepository.java
@@ -3,10 +3,27 @@ package com.jpaProject.jpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.jpaProject.jpa.models.Author;
 
+import jakarta.transaction.Transactional;
+
 public interface AuthorRepository extends JpaRepository<Author,Integer> {
+
+    // update Author set a.age = :age where a.id = :id
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age where a.id = :id")
+    int updateAuthor(int age, int id);
+
+    // update Author set a.age = 24
+    @Modifying
+    @Transactional
+    @Query("update Author a set a.age = :age")
+    int updateAllAuthorsAge(int age);
+
     // select * from Author where first_name = fn
     List<Author> findAllByFirstName(String fn);
     
